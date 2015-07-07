0c0f2da4dba0df94d5d0e712e1ce201e10ae1215
Santiago Zavala <santiago1717@gmail.com>
2015-07-07 17:15:47 -0500

    Thanks to AL now every request is auth

--- api_schema.js
+++ api_schema.js
@@ -11,61 +11,61 @@
 
 module.exports = {
   'get': [
-    {name: "Feed", path: "/feed", optional: ["personalized", "since"]},
+    {name: "Feed", path: "/feed", optional: ["personalized", "since"], auth: true},
 
-    {name: "Comments", path: "/comments", required: ["commentable_type", "commentable_id"]},
+    {name: "Comments", path: "/comments", required: ["commentable_type", "commentable_id"], auth: true},
 
-    {name: "Users", path: "/users/:id", placeholder: ["id"], optional: ["include_details"]},
-    {name: "UsersRoles", path: "/users/:id/roles", placeholder: ["id"]},
-    {name: "UsersBatch", path: "/users", required: ["ids"]},
-    {name: "UsersSearch", path: "/users", optional: ["slug", "md5"]},
+    {name: "Users", path: "/users/:id", placeholder: ["id"], optional: ["include_details"], auth: true},
+    {name: "UsersRoles", path: "/users/:id/roles", placeholder: ["id"], auth: true},
+    {name: "UsersBatch", path: "/users", required: ["ids"], auth: true},
+    {name: "UsersSearch", path: "/users", optional: ["slug", "md5"], auth: true},
     {name: "Me", path: "/me", auth: true},
 
-    {name: "UsersFollowers", path: "/users/:id/followers", placeholder: ["id"]},
-    {name: "UsersFollowersIds", path: "/users/:id/followers/ids", placeholder: ["id"]},
-    {name: "UsersFollowing", path: "/users/:id/following", placeholder: ["id"], optional: ["type"]},
-    {name: "UsersFollowingIds", path: "/users/:id/following/ids", placeholder: ["id"], optional: ["type"]},
-    {name: "FollowsRelationship", path: "/follows/relationship", required: ["source_id", "target_type", "target_id"]},
-    {name: "FollowsBatch", path: "/follows/batch", required: ["ids"]},
+    {name: "UsersFollowers", path: "/users/:id/followers", placeholder: ["id"], auth: true},
+    {name: "UsersFollowersIds", path: "/users/:id/followers/ids", placeholder: ["id"], auth: true},
+    {name: "UsersFollowing", path: "/users/:id/following", placeholder: ["id"], optional: ["type"], auth: true},
+    {name: "UsersFollowingIds", path: "/users/:id/following/ids", placeholder: ["id"], optional: ["type"], auth: true},
+    {name: "FollowsRelationship", path: "/follows/relationship", required: ["source_id", "target_type", "target_id"], auth: true},
+    {name: "FollowsBatch", path: "/follows/batch", required: ["ids"], auth: true},
 
-    {name: "Jobs", path: "/jobs"},
-    {name: "Job", path: "/jobs/:id", placeholder: ["id"]},
+    {name: "Jobs", path: "/jobs", auth: true},
+    {name: "Job", path: "/jobs/:id", placeholder: ["id"], auth: true},
 
-    {name: "Likes", path: "/likes", required: ["likable_type", "likable_id"]},
+    {name: "Likes", path: "/likes", required: ["likable_type", "likable_id"], auth: true},
 
     {name: "Messages", path: "/messages", optional: ["view"], auth: true},
     {name: "MessagesThread", path: "/messages/:id", placeholder: ["id"], auth: true},
 
     {name: "Paths", path: "/paths", optional: ["user_ids", "startup_ids", "direction"], auth: true},
 
-    {name: "StartupPress", path: "/press", required: ["startup_id"]},
-    {name: "Press", path: "/press/:id", placeholder: ["id"]},
+    {name: "StartupPress", path: "/press", required: ["startup_id"], auth: true},
+    {name: "Press", path: "/press/:id", placeholder: ["id"], auth: true},
 
-    {name: "Reviews", path: "/reviews", optional: ["user_id"]},
-    {name: "Review", path: "/reviews/:id", placeholder: ["id"]},
+    {name: "Reviews", path: "/reviews", optional: ["user_id"], auth: true},
+    {name: "Review", path: "/reviews/:id", placeholder: ["id"], auth: true},
 
-    {name: "Search", path: "/search", required: ["query"], optional: ["type"]},
-    {name: "SearchSlugs", path: "/search/slugs", required: ["query"]},
+    {name: "Search", path: "/search", required: ["query"], optional: ["type"], auth: true},
+    {name: "SearchSlugs", path: "/search/slugs", required: ["query"], auth: true},
 
-    {name: "Startups", path: "/startups/:id", placeholder: ["id"]},
-    {name: "StartupsComments", path: "/startups/:id/comments", placeholder: ["id"]},
-    {name: "StartupsRoles", path: "/startups/:id/roles", placeholder: ["id"], optional: ["direction"]},
-    {name: "StartupsFollowers", path: "/startups/:id/followers", placeholder: ["id"]},
-    {name: "StartupsFollowersIds", path: "/startups/:id/followers/ids", placeholder: ["id"]},
-    {name: "StartupsJobs", path: "/startups/:id/jobs", placeholder: ["id"]},
-    {name: "StartupsBatch", path: "/startups/batch", required: ["ids"]},
-    {name: "StartupsSearch", path: "/startups/search", optional: ["slug", "domain"]},
+    {name: "Startups", path: "/startups/:id", placeholder: ["id"], auth: true},
+    {name: "StartupsComments", path: "/startups/:id/comments", placeholder: ["id"], auth: true},
+    {name: "StartupsRoles", path: "/startups/:id/roles", placeholder: ["id"], optional: ["direction"], auth: true},
+    {name: "StartupsFollowers", path: "/startups/:id/followers", placeholder: ["id"], auth: true},
+    {name: "StartupsFollowersIds", path: "/startups/:id/followers/ids", placeholder: ["id"], auth: true},
+    {name: "StartupsJobs", path: "/startups/:id/jobs", placeholder: ["id"], auth: true},
+    {name: "StartupsBatch", path: "/startups/batch", required: ["ids"], auth: true},
+    {name: "StartupsSearch", path: "/startups/search", optional: ["slug", "domain"], auth: true},
 
-    {name: "Tags", path: "/tags/:id", placeholder: ["id"]},
-    {name: "TagsChildren", path: "/tags/:id/children", placeholder: ["id"]},
-    {name: "TagsParents", path: "/tags/:id/parents", placeholder: ["id"]},
-    {name: "TagsJobs", path: "/tags/:id/jobs", placeholder: ["id"]},
-    {name: "TagsStartups", path: "/tags/:id/startups", placeholder: ["id"], optional: ["order"]},
-    {name: "TagsUsers", path: "/tags/:id/users", placeholder: ["id"], optional: ["include_children", "include_parents", "investors"]},
+    {name: "Tags", path: "/tags/:id", placeholder: ["id"], auth: true},
+    {name: "TagsChildren", path: "/tags/:id/children", placeholder: ["id"], auth: true},
+    {name: "TagsParents", path: "/tags/:id/parents", placeholder: ["id"], auth: true},
+    {name: "TagsJobs", path: "/tags/:id/jobs", placeholder: ["id"], auth: true},
+    {name: "TagsStartups", path: "/tags/:id/startups", placeholder: ["id"], optional: ["order"], auth: true},
+    {name: "TagsUsers", path: "/tags/:id/users", placeholder: ["id"], optional: ["include_children", "include_parents", "investors"], auth: true},
 
-    {name: "StartupRoles", path: "/startup_roles", required: ["v"], optional: ["user_id", "startup_id", "role", "direction"]},
+    {name: "StartupRoles", path: "/startup_roles", required: ["v"], optional: ["user_id", "startup_id", "role", "direction"], auth: true},
 
-    {name: "StatusUpdates", path: "/status_updates", optional: ["user_id", "startup_id"]},
+    {name: "StatusUpdates", path: "/status_updates", optional: ["user_id", "startup_id"], auth: true},
 
     {name: "TalentStartups", path: "/talent/startups", auth: true},
     {name: "TalentCandidates", path: "/talent/candidates", required: ["startup_id"], auth: true}
@@ -87,5 +87,3 @@ module.exports = {
     {name: "StatusUpdates", path: "/status_updates/:id", placeholder: ["id"], auth: true}
   ]
 };
-
-
